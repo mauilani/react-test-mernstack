4a1e19c82b21b17be9719c6717e7767db232c3ec
mauilani <mauilanilocomoco@gmail.com>
2023-01-06 18:52:09 +0900

    dev-api routes/auth.js users.js Change

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -11,13 +11,13 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: hashedPass,
+      password: hashedPass
     });
 
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(eer);
+    res.status(500).json(err);
   }
 });
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,24 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 //UPDATE
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
     try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updateUser);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -15,5 +28,36 @@ router.put("/:id", async (req, res) => {
 });
 
 //DELETE
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("user has been deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found");
+    }
+  } else {
+    res.status(401).json("You can delete only your account");
+  }
+});
+
+//GET USER
+//user情報を取得する処理
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
